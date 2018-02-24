d37873bb6f14ce673de728ffe284044cbdee6c8c
Ciprian Bindiu <ciprian.bindiu@gmail.com>
2018-02-24 15:22:11 +0200

    Create activity_details landscape layout Correct activity_details portrait layout

--- app/src/main/java/com/future/bestmovies/DetailsActivity.java
+++ app/src/main/java/com/future/bestmovies/DetailsActivity.java
@@ -16,14 +16,7 @@ import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
     public static final String MOVIE_OBJECT = "movie";
-
-    // TODO: 1
-    private static final String SANDWICH_NAME = "name";
-    private static final String SANDWICH_IMAGE_URL = "image";
-    private static final String SANDWICH_ORIGIN = "origin";
-    private static final String SANDWICH_AKA = "also_known_as";
-    private static final String SANDWICH_DESCRIPTION = "description";
-    private static final String SANDWICH_INGREDIENTS = "ingredients";
+    private Movie mSelectedMovie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,50 +24,48 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ImageView movieBackdropImageView = (ImageView) findViewById(R.id.details_backdrop_iv);
-        TextView movieTitleTextView = (TextView) findViewById(R.id.details_title_tv);
-        ImageView moviePosterImageView = (ImageView) findViewById(R.id.details_poster_iv);
-        TextView moviePlotTextView = (TextView) findViewById(R.id.details_plot_tv);
-        TextView movieRatingTextView = (TextView) findViewById(R.id.details_ratings_tv);
-        TextView movieReleaseDateTextView = (TextView) findViewById(R.id.details_release_date_tv);
+        ImageView movieBackdropImageView = findViewById(R.id.details_backdrop_iv);
+        TextView movieTitleTextView = findViewById(R.id.details_title_tv);
+        ImageView moviePosterImageView = findViewById(R.id.details_poster_iv);
+        TextView moviePlotTextView = findViewById(R.id.details_plot_tv);
+        TextView movieRatingTextView = findViewById(R.id.details_ratings_tv);
+        TextView movieReleaseDateTextView = findViewById(R.id.details_release_date_tv);
 
-        //getSupportActionBar().setElevation(0f);
-        if (savedInstanceState != null) {
-            // TODO: 2
-
+        if (savedInstanceState != null && savedInstanceState.containsKey(MOVIE_OBJECT)) {
+            mSelectedMovie = savedInstanceState.getParcelable(MOVIE_OBJECT);
         } else {
             Intent intent = getIntent();
             if (intent == null || !intent.hasExtra(MOVIE_OBJECT)) {
                 closeOnError();
             }
 
-            Movie selectedMovie = intent.getParcelableExtra(MOVIE_OBJECT);
-
-            if (selectedMovie == null) {
-                // Movies data unavailable
-                closeOnError();
-                return;
-            }
-
-            String basePath = "http://image.tmdb.org/t/p/w500";
-
-            String backdropUrl = basePath.concat(selectedMovie.getBackdropPath());
-            Picasso.with(this)
-                    .load(backdropUrl)
-                    .into(movieBackdropImageView);
-
-            movieTitleTextView.setText(selectedMovie.getMovieTitle());
-            movieRatingTextView.setText(String.valueOf(selectedMovie.getVoteAverage()));
-            movieReleaseDateTextView.setText(selectedMovie.getReleaseDate());
-
-            String basePosterPath = "http://image.tmdb.org/t/p/w185";
-            String posterUrl = basePosterPath.concat(selectedMovie.getPosterPath());
-            Picasso.with(this)
-                    .load(posterUrl)
-                    .into(moviePosterImageView);
-
-            moviePlotTextView.setText(selectedMovie.getOverview());
+            mSelectedMovie = intent.getParcelableExtra(MOVIE_OBJECT);
         }
+
+        if (mSelectedMovie == null) {
+            // Movies data unavailable
+            closeOnError();
+            return;
+        }
+
+        String basePath = "http://image.tmdb.org/t/p/w500";
+
+        String backdropUrl = basePath.concat(mSelectedMovie.getBackdropPath());
+        Picasso.with(this)
+                .load(backdropUrl)
+                .into(movieBackdropImageView);
+
+        movieTitleTextView.setText(mSelectedMovie.getMovieTitle());
+        movieRatingTextView.setText(String.valueOf(mSelectedMovie.getVoteAverage()));
+        movieReleaseDateTextView.setText(mSelectedMovie.getReleaseDate());
+
+        String basePosterPath = "http://image.tmdb.org/t/p/w185";
+        String posterUrl = basePosterPath.concat(mSelectedMovie.getPosterPath());
+        Picasso.with(this)
+                .load(posterUrl)
+                .into(moviePosterImageView);
+
+        moviePlotTextView.setText(mSelectedMovie.getOverview());
     }
 
     @Override
@@ -95,15 +86,7 @@ public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelable(MOVIE_OBJECT, mSelectedMovie);
         super.onSaveInstanceState(outState);
-
-        // TODO: 3
-        // Put the contents of the Views into the outState Bundle.
-//        outState.putString(SANDWICH_NAME, getTitle().toString());
-//        outState.putString(SANDWICH_IMAGE_URL, mImageUrl);
-//        outState.putString(SANDWICH_ORIGIN, mOriginTv.getText().toString());
-//        outState.putString(SANDWICH_AKA, mAlsoKnownTv.getText().toString());
-//        outState.putString(SANDWICH_DESCRIPTION, mDescriptionTv.getText().toString());
-//        outState.putString(SANDWICH_INGREDIENTS, mIngredientsTv.getText().toString());
     }
 }
